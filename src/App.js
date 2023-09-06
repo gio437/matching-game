884ae82386af8797f2ffb88e216addfaaa8e02b5
Giovanni <giomail33@gmail.com>
2023-09-06 15:52:41 -0400

    added colors to signify when a description is selected

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef, createRef } from 'react';
 import Belgian from "./pictures/belgian.png";
 // import Tibetan from './pictures/tibetan.png';
 // import Otterhound from './pictures/otterhound.png';
@@ -10,6 +10,28 @@ import Briard from './pictures/briard.png';
 function App() {
   let [picturesArray, setPicturesArray] = useState([1, 2, 3]);
   let [descriptionArray, setDescriptionArray] = useState([]);
+
+  function activateInputBox() {
+    const inputSquare = document.querySelectorAll('.inputParent');
+    inputSquare.forEach(box => {
+      box.style.backgroundColor = 'yellow';
+    })
+  }
+
+  function placeDescription() {
+    const inputSquare = document.querySelectorAll('.inputParent');
+    inputSquare.forEach(box => {
+      box.style.backgroundColor = 'darkblue';
+    })
+  }
+
+// add in new descriptions
+  const descriptions = {
+    norwegianDescription: "A small dog breed of the Spitz type that originates from Norway. Its name is a compound noun composed of the elements lunde, meaning puffin (Norwegian lunde, puffin, or lundefugl, puffin bird), and hund, meaning dog. The breed was originally developed for the hunting of puffins and their eggs on inaccessible nesting places in caves and on cliffs. The breed was at the brink of extinction in the 1960s and preservation efforts have since been underway.",
+    belgianDescription: "A breed of medium-sized herding dog from Belgium. While predominantly considered a single breed, it is bred in four distinct varieties based on coat type and colour; the long-haired black Groenendael, the rough-haired fawn Laekenois, the short-haired fawn Malinois, and the long-haired fawn Tervuren. In the United States, the American Kennel Club considers the four varieties to be separate breeds.",
+    briardDescription: "a French breed of large shepherd dog, traditionally used both for herding sheep and to defend them. It was first shown at the first Paris dog show, in 1863; the first Briard to be registered in the Livre des Origines Francaises, the national stud-book, was Sans Gene in 1885. It was in the past also known as the Chien de Berger francais de Plaine."
+  }
+
 
   function endGame() {
     if (descriptionArray.length === 3) {
@@ -27,15 +49,15 @@ function App() {
         <img className='picBox' src={Norwegian}></img>
         <img className='picBox' src={Briard}></img>
       </div>
-      <div className='inputParent'>
+      <div className='inputParent' onClick={placeDescription}>
         <div className='inputBox' id='whiteBox'></div>
         <div className='inputBox' id='whiteBox'></div>
         <div className='inputBox' id='whiteBox'></div>
       </div>
-      <div className='descriptionParent'>
-        <div className='descriptionBox' id='whiteBox'>A breed of medium-sized herding dog from Belgium. While predominantly considered a single breed, it is bred in four distinct varieties based on coat type and colour; the long-haired black Groenendael, the rough-haired fawn Laekenois, the short-haired fawn Malinois, and the long-haired fawn Tervuren. In the United States, the American Kennel Club considers the four varieties to be separate breeds.</div>
-        <div className='descriptionBox' id='whiteBox'>A small dog breed of the Spitz type that originates from Norway. Its name is a compound noun composed of the elements lunde, meaning puffin (Norwegian lunde, "puffin", or lundefugl, "puffin bird"), and hund, meaning dog. The breed was originally developed for the hunting of puffins and their eggs on inaccessible nesting places in caves and on cliffs. The breed was at the brink of extinction in the 1960s and preservation efforts have since been underway</div>
-        <div className='descriptionBox' id='whiteBox'>a French breed of large shepherd dog, traditionally used both for herding sheep and to defend them. It was first shown at the first Paris dog show, in 1863; the first Briard to be registered in the Livre des Origines Francaises, the national stud-book, was Sans Gene in 1885. It was in the past also known as the Chien de Berger francais de Plaine.</div>
+      <div className='descriptionParent' onClick={activateInputBox}>
+        <div className='descriptionBox' id='whiteBox'>{descriptions.norwegianDescription}</div>
+        <div className='descriptionBox' id='whiteBox'>{descriptions.belgianDescription}</div>
+        <div className='descriptionBox' id='whiteBox'>{descriptions.briardDescription}</div>
       </div>
     </div>
   );
